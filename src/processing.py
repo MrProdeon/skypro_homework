a2977928e325e5acd3e19a6179f7f67e9823a9ac
Prodeon <prodeon21@gmail.com>
2025-09-18 11:36:44 +0300

    use linters for processing.py for func fulter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
-def filter_by_state(list_of_dicts : list[dict], state : str ='EXECUTED') -> list[dict]:
-    filtered_by_state_lists = [current_dict for current_dict in list_of_dicts
-                               if 'state' in current_dict
-                               and current_dict['state'] == state]
-    return filtered_by_state_lists
+def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
+    filtered_by_state_lists = [
+        current_dict for current_dict in list_of_dicts if "state" in current_dict and current_dict["state"] == state
+    ]
 
+    return filtered_by_state_lists
