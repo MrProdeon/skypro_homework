642a8f04c64309f715d2007f82ba7402d88c6b96
Prodeon <prodeon21@gmail.com>
2025-09-23 18:00:47 +0300

    complete tests for module masks.py

---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+from src.masks import get_mask_account, get_mask_card_number
+import pytest
+
+
+def test_get_mask_account(get_account_1, get_account_2, get_account_3):
+    assert get_mask_account(get_account_1) == '**3750'
+
+    assert get_mask_account(get_account_2) == '**2016'
+
+    assert get_mask_account(get_account_3) == '**9324'
+
+
+def test_get_mask_card_number(get_card_number_1, get_card_number_2, get_card_number_3):
+    assert get_mask_card_number(get_card_number_1) == '4242 42** **** 4242'
+
+    assert get_mask_card_number(get_card_number_2) == '5555 55** **** 4444'
+
+    assert get_mask_card_number(get_card_number_3) == '3891 72** **** 3456'
+
+
+
