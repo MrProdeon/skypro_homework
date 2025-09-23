358d47b5ac40f07c41d2d40a917fa639153d5995
Prodeon <prodeon21@gmail.com>
2025-09-23 18:35:28 +0300

    delete doubles tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,28 +25,3 @@ def get_card_number_2():
 def get_card_number_3():
     return 3891725234553456
 import pytest
-
-@pytest.fixture
-def get_account_1():
-    return '48291530670418293750'
-
-@pytest.fixture
-def get_account_2():
-    return '70913428560194732016'
-
-@pytest.fixture
-def get_account_3():
-    return '35678049281756019324'
-
-
-@pytest.fixture
-def get_card_number_1():
-    return 4242424242424242
-
-@pytest.fixture
-def get_card_number_2():
-    return 5555555555554444
-
-@pytest.fixture
-def get_card_number_3():
-    return 3891725234553456
