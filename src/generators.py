2d8b8e276a4066181f764862b62aae0226844ef8
Prodeon <prodeon21@gmail.com>
2025-09-27 08:49:15 +0300

    add module generators and func filter_by_currency

---
+++ src/generators.py
@@ -0,0 +1,11 @@
+def filter_by_currency(transactions : list[dict], currency : str = 'USD') -> iter :
+    """
+    Функция для отбора транзакций по заданной валюте.
+    Функция принимает список транзакций в виде списка словарей и валюту, по умолчанию USD
+    Вернет итератор, в котором будут только словари, которые имеют только нужная валюта.
+    """
+    filtered_transactions = (one_transaction for one_transaction in transactions
+                             if one_transaction['currency']['name'] == currency)
+
+    for transaction in filtered_transactions:
+        yield transaction
