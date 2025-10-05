5ae7fa7804f2799d0dcaa1b76725e7d063c1ecd7
Prodeon <prodeon21@gmail.com>
2025-10-05 09:36:57 +0300

    add new module decorators and add new decorator log

---
+++ src/decorators.py
@@ -0,0 +1,38 @@
+from functools import wraps
+import os
+
+
+def log(filename=None):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            func_name = func.__name__
+
+            try:
+                result = func(*args, **kwargs)
+                message = f'Успех! Имя функции : {func_name}\nРезультат : {result}'
+            except Exception as e:
+                arguments = args, kwargs
+                error = e
+                message = f'Исключение!\nИмя функции : {func_name}\nОшибка : {error}\nВходные параметры : {arguments}'
+                result = None
+            if filename:
+                name_file = os.path.join(os.path.dirname(os.path.dirname(__file__)),'data', filename)
+                with open(name_file, 'a', encoding='UTF-8') as file:
+                    file.write(message + '\n')
+            else:
+                print(message)
+            return result
+
+
+
+
+        return inner
+    return wrapper
+
+
+@log('testing.txt')
+def get_sum(a, b):
+    return a + b
+
+print(get_sum(1, 'a'))
