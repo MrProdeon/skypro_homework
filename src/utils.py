03f41b150dd22f3079db06542ae1022c48d612a9
Prodeon <prodeon21@gmail.com>
2025-10-20 15:05:59 +0300

    write logger and logs in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
 import os
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("../logs/logs.log", 'w', encoding="UTF-8")
+formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def get_info_about_operation(path_to_file: str) -> list[dict] | list:
@@ -10,17 +18,21 @@ def get_info_about_operation(path_to_file: str) -> list[dict] | list:
     2) Вернулся объект, который не является списком
     3) Файл не найден
     """
-
+    logger.info('Начинаем преобразование')
     try:
         if os.path.getsize(path_to_file) == 0:
+            logger.info("Получен пустой файл, возвращаем пустой список")
             return []
 
         with open(path_to_file, "r", encoding="UTF-8") as file:
             json_file = json.load(file)
             if not isinstance(json_file, list):
+                logger.info("Объект не является списком, возвращаем пустой список")
                 return []
 
+            logger.info("JSON-файл был успешно преоразован в Python-объект")
             return json_file
 
     except FileNotFoundError:
+        logger.exception("Файл не найден")
         return []
