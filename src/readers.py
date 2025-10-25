2b3053f836772201020d2ea8f60b810bc7ca34eb
Prodeon <prodeon21@gmail.com>
2025-10-25 11:29:17 +0300

    add delimiter in readers/csv_reader

--- src/readers.py
+++ src/readers.py
@@ -3,7 +3,8 @@ import os
 
 import pandas as pd
 
-path = os.path.join(os.path.dirname(__file__), "..", "data", "transactions_excel.xlsx")
+path_csv = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
+path_excel = os.path.join(os.path.dirname(__file__), "..", "data", "transactions_excel.xlsx")
 
 
 def csv_reader(path_to_csv_file: str) -> list[dict]:
@@ -13,8 +14,8 @@ def csv_reader(path_to_csv_file: str) -> list[dict]:
     :return: Список словарей, в котором каждый словарь - это строка из csv файла, где ключ - название столбца, а
     значение - значение ячейки в строке по этому столбцу
     """
-    with open(path_to_csv_file, "r", encoding="UTF-8") as file:
-        reader = csv.DictReader(file)
+    with open(path_to_csv_file, "r", encoding="UTF-8",) as file:
+        reader = csv.DictReader(file, delimiter=";")
         return [row for row in reader]
 
 
@@ -27,3 +28,5 @@ def excel_reader(path_to_excel_file: str) -> list[dict]:
     """
     reader_dataframe = pd.read_excel(path_to_excel_file)
     return reader_dataframe.to_dict(orient="records")
+
+print(csv_reader(path_csv))
